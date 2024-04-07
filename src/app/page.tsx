b6a01193a076ae8e0d9df5f823b2f5c3f657ae58
JohnFuhrm12 <61069716+JohnFuhrm12@users.noreply.github.com>
2024-04-07 20:50:05 -0300

    Installed Material UI and Langchain

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Button from '@mui/material/Button';
+
+import { ChatOpenAI } from "langchain/chat_models/openai";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <h2>INIT</h2>
+      <Button variant="contained">Click Me</Button>
     </main>
   );
 }
