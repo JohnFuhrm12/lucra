fd109fa5630e434f5053f5b3dd77922bea38e1b2
JohnFuhrm12 <61069716+JohnFuhrm12@users.noreply.github.com>
2024-04-10 13:53:09 -0300

    Added Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from 'next/script';
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const G_TAG = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS;
 
 export const metadata: Metadata = {
   title: "Lucra AI App",
@@ -16,6 +19,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-1F292B16GB"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', ${G_TAG});
+        `}
+      </Script>
       <body className={inter.className}>{children}</body>
     </html>
   );
