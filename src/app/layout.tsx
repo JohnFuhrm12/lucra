f5c2ff554deed676b288edf05969b83ac0b99efd
JohnFuhrm12 <61069716+JohnFuhrm12@users.noreply.github.com>
2024-04-10 14:04:11 -0300

    Removed Google Analytics, added Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Script from 'next/script';
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
-
-const G_TAG = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS;
 
 export const metadata: Metadata = {
   title: "Lucra AI App",
@@ -19,20 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id='G-1F292B16GB`}
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-        
-          gtag('config', 'G-1F292B16GB);
-        `}
-      </Script>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics/>
+      </body>
     </html>
   );
 }
